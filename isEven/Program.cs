803c8a896cba3682da9af463a385e3a0cbccd7c7
Maria Fadeeva <4803328@gmail.com>
2023-06-08 23:25:00 +0300

    add is even example

---
+++ isEven/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string n = Console.ReadLine();
+int userNumber = int.Parse(n);
+bool isEven = userNumber%2 == 0;
+
+if (isEven)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
+
